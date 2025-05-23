49a2a7f3697f4ba7fc454fae1cddce8d41c2100c
Adrián Bolonio <adrian.bolonio@kununu.com>
2025-05-23 15:38:37 +0200

    Update utils

---
+++ src/utils/getAllElements/index.ts
@@ -0,0 +1,11 @@
+/**
+ * This is a utility function that returns the name of the elements to be generated.
+ *
+ * @format
+ * @param {string} name - The name of the elements to be generated.
+ * @returns {string} The name of the elements to be generated.
+ */
+
+export const getAllElements = (name: string) => {
+  return name;
+};
