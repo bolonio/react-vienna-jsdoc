fb09d3c70d1e901eda24a6036e5cd67da20dd164
Adrián Bolonio <adrian.bolonio@kununu.com>
2025-05-23 15:39:56 +0200

    Update util

--- src/utils/getElements/index.ts
+++ src/utils/getElements/index.ts
@@ -2,7 +2,7 @@
  * This is a utility function that generates an array of elements with a specified name and number.
  *
  * @param {string} name - The name of the elements to be generated.
- * @param {number} n- The number of elements to be generated.
+ * @param {number} n - The number of elements to be generated.
  * @returns {string[]} An array of elements with the specified name and number.
  */
 
